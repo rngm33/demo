7db6ad4ce42ece84f2fcda74b7ad6944c51ef3de
Rabin Gautam <rabingautam22@gmail.com>
2019-11-21 11:51:15 +0545

    aa

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('DB_HOST', 'https://phplaravel-344509-1064601.cloudwaysapps.com/'),
+            'port' => env('DB_PORT', '8082'),
+            'database' => env('DB_DATABASE', 'xrxttwwurh'),
+            'username' => env('DB_USERNAME', 'forgexrxttwwurh'),
+            'password' => env('DB_PASSWORD', 'C48Jh3mGke'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
